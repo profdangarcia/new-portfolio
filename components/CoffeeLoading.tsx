bf7affb54cb758376e28e0da656d90c83c8590dc
Dan Garcia <daniel.of@gmail.com>
2026-03-12 13:59:45 -0300

    Update design system styles

--- components/CoffeeLoading.tsx
+++ components/CoffeeLoading.tsx
@@ -54,7 +54,7 @@ export default function CoffeeLoading() {
       {/* Wrapper centralizado por flexbox (mais estável no mobile) */}
       <div className="flex shrink-0 items-center justify-center">
         <div
-          className="relative h-[100px] w-[140px] rounded-b-[50px] border-[6px] border-[#111] bg-white shadow-[0_0_0_6px_white] md:h-[140px] md:w-[180px] md:rounded-b-[70px]"
+          className="relative h-[6.25rem] w-[8.75rem] rounded-b-[3.125rem] border-[0.375rem] border-[#111] bg-white shadow-[0_0_0_0.375rem_white] md:h-[8.75rem] md:w-[11.25rem] md:rounded-b-[4.375rem]"
           style={{
             transform: showCup ? "translateX(0)" : "translateX(-100vw)",
             transition: "transform 1s ease-in-out",
@@ -62,12 +62,12 @@ export default function CoffeeLoading() {
         >
           {/* Coffee fill animation */}
           <div
-            className={`absolute inset-x-[6px] bottom-[6px] rounded-b-[44px] bg-[#3d2314] md:rounded-b-[64px] ${showCup ? "coffee-fill" : ""}`}
+            className={`absolute inset-x-[0.375rem] bottom-[0.375rem] rounded-b-[2.75rem] bg-[#3d2314] md:rounded-b-[4rem] ${showCup ? "coffee-fill" : ""}`}
             style={{ height: showCup ? undefined : "0%" }}
           />
           {/* Handle */}
           <div
-            className="absolute left-[146px] top-[-2px] z-[100] h-[50px] w-[35px] rounded-r-lg border-[6px] border-white border-l-transparent bg-transparent md:left-[186px] md:top-[2px] md:h-[70px] md:w-[40px] md:rounded-r-3xl"
+            className="absolute left-[9.125rem] top-[-0.125rem] z-[100] h-[3.125rem] w-[2.1875rem] rounded-r-lg border-[0.375rem] border-white border-l-transparent bg-transparent md:left-[11.625rem] md:top-[0.125rem] md:h-[4.375rem] md:w-[2.5rem] md:rounded-r-3xl"
             aria-hidden
           />
         </div>
